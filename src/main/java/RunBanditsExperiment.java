57bb2871f1f3a887a26db3dc34cf037d81fd807d
charlesANC <charles.costa@gmail.com>
2021-10-01 09:22:49 -0300

    Parametros para execucao

--- src/main/java/RunBanditsExperiment.java
+++ src/main/java/RunBanditsExperiment.java
@@ -1,14 +1,50 @@
 import java.security.InvalidParameterException;
 
+import br.unb.cic.comnet.bandits.environment.GeneralParameters;
+
 public class RunBanditsExperiment {
 
 	public static void main(String[] args) {
+		System.out.println("RunBanditsExperiment - Simulation of bandits experiments written in JADE.");		
 		try {
+			if (args.length < 4) {
+				System.out.print("Use java -jar RunBandits.jar Algorithm useTrust honestWitnesses cooptedWitnesses ");
+				System.out.println("[numOfRounds][epsilon][outputDirectory] ");
+				System.out.println("   Where: ");
+				System.out.println("      - Algorithm: Can be {epsilon_greedy, epsilon_first, epsilon_decreasing, ucb1, exp1}");
+				System.out.println("      - useTrust: T if evaluate using FIRE T&RM or N if using simple average ");
+				System.out.println("      - honestWitnesses: Number of honest witnesses agents to be created.");
+				System.out.println("      - cooptedWitnesses: Number of coopted witnesses agents to be created.");
+				System.out.println("   Optionally: ");
+				System.out.println("      - numOfRounds: Number of rounds to be played. 1000, by default.");
+				System.out.println("      - epsilon: Value of epsilon. 0.8, by default.");
+				System.out.println("      - outputDirectory: Directory where logs will be written. c:\\temp, by default.");
+				return;
+			}
+			System.out.print("---");
+			System.out.print("");			
+			
 			String banditAlgorithm = args[0];
 			String useTrust = args[1];			
 			int honestWitnesses = Integer.valueOf(args[2]);
 			int cooptedWitnesses = Integer.valueOf(args[3]);
+			
+			Long numOfRounds = null;
+			if (args.length >= 5) {
+				numOfRounds = Long.valueOf(args[4]);
+			}
+			
+			Double epsilon = null;
+			if (args.length >= 6) {
+				epsilon = Double.valueOf(args[5]);
+			}			
 
+			String outputDirectory = null;			
+			if (args.length >= 7) {
+				outputDirectory = args[6];
+			}			
+
+			GeneralParameters.initilizeParameters(outputDirectory, numOfRounds, epsilon);
 			
 			jade.Boot.main(configuracao(banditAlgorithm, useTrust, honestWitnesses, cooptedWitnesses));
 		} catch (InvalidParameterException e) {
